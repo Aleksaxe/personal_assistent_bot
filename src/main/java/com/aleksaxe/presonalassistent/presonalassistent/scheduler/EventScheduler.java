dedd8ca889f10fa7e8f76a7243aeb75133abb16d
Аксенов Александр <a.aksenov@rosdomofon.com>
2023-08-23 01:14:49 +0600

    cache fix

--- src/main/java/com/aleksaxe/presonalassistent/presonalassistent/scheduler/EventScheduler.java
+++ src/main/java/com/aleksaxe/presonalassistent/presonalassistent/scheduler/EventScheduler.java
@@ -1,6 +1,7 @@
 package com.aleksaxe.presonalassistent.presonalassistent.scheduler;
 
 import com.aleksaxe.presonalassistent.presonalassistent.services.PersonalAssistantBot;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -21,5 +22,10 @@ public class EventScheduler {
     @Scheduled(cron = "0 0 5 * * ?")
     public void clearEventsTask() {
         personalAssistantBot.clearEvents();
+    }
+
+    @CacheEvict(value = "closeEventsCache", allEntries = true)
+    @Scheduled(fixedRateString = "${caching.spring.closeEventsCache.evictRate}")
+    public void evictCloseEventsCache() {
     }
 }
